3c09592dbcbd83fe3199ec98062c4f295315e378
Ali Altunoğlu <alialtunoglu02@gmail.com>
2025-02-11 11:36:22 +0300

    inital commit

---
+++ Infrastructure/MovieApi.Persistance/Context/MovieContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+namespace MovieApi.Persistance.Context
+{
+    public class MovieContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;initial Catalog=ApiMovieDb;Trusted_Connection=True;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+
+    }
+}
