ea5144ebda2b9959a72367e600230b973c05782a
Ali Altunoğlu <alialtunoglu02@gmail.com>
2025-03-08 16:51:05 +0300

    regularization .gitignore

--- Core/MovieApi.Domain/obj/Debug/net9.0/MovieApi.Domain.AssemblyInfo.cs
+++ Core/MovieApi.Domain/obj/Debug/net9.0/MovieApi.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApi.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fe7d2f8d8b5f5cb53246c92614e91b46e0ff563")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b57d7f51c4cafeae7093a373a0a02567ad7454f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApi.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApi.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
